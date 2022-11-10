aab28e3c3246834c99cce3f8999c0b046771e778
MrSallivan <nik.sallivan@gmail.com>
2022-11-10 19:57:59 +1000

    Update 8.js

--- JS/Basic/8.js
+++ JS/Basic/8.js
@@ -7,3 +7,8 @@ myStr += " This is the second sentence."
 // Only change code below this line
 const myName = "Alex";
 const myStr2 = "My name is" + myName + "and and I am well!";
+
+// Change code below this line
+const someAdjective = "asd";
+let myStr = "Learning to code is ";
+myStr += someAdjective
