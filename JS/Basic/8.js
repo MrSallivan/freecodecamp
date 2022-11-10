fdae35011d8d6a3f0dea60f1cb8a6dc688a0848e
MrSallivan <nik.sallivan@gmail.com>
2022-11-10 19:51:50 +1000

    Update 8.js

--- JS/Basic/8.js
+++ JS/Basic/8.js
@@ -1,1 +1,5 @@
-const myStr = "This is the start." + " This is the end."; // Change this line
+const myStr1 = "This is the start." + " This is the end."; // Change this line
+
+let myStr;
+myStr = "This is the first sentence."
+myStr += " This is the second sentence."
