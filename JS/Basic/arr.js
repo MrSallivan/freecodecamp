12ec2980d41f5a65c7ff37d2c98ea2dba6da8da8
MrSallivan <nik.sallivan@gmail.com>
2022-11-11 07:31:06 +1000

    Update arr.js

--- JS/Basic/arr.js
+++ JS/Basic/arr.js
@@ -29,3 +29,21 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 let removedFromMyArray = myArray.pop()
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+let removedFromMyArray = myArray.shift()
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35])
+
+function functionWithArgs(p1, p2){
+  console.log(p1, p2)
+}
+functionWithArgs('Fuck', 'off')
