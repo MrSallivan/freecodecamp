b9d15f2433830bb75bda8a326bf09d4f0e6f773c
MrSallivan <nik.sallivan@gmail.com>
2022-11-10 20:41:01 +1000

    Update arr.js

--- JS/Basic/arr.js
+++ JS/Basic/arr.js
@@ -3,3 +3,9 @@ const myArray = ['string2', "string1", 4];
 
 const myArray = [50, 60, 70];
 let myData = myArray[0]
+
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45
+// Only change code below this line
+
