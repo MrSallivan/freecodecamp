70a28600128baccd7e4f73760ccff947a856123c
MrSallivan <nik.sallivan@gmail.com>
2022-11-10 19:54:11 +1000

    Update 8.js

--- JS/Basic/8.js
+++ JS/Basic/8.js
@@ -3,3 +3,7 @@ const myStr1 = "This is the start." + " This is the end."; // Change this line
 let myStr;
 myStr = "This is the first sentence."
 myStr += " This is the second sentence."
+
+// Only change code below this line
+const myName = "Alex";
+const myStr2 = "My name is" + myName + "and and I am well!";
