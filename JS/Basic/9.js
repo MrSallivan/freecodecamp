9ff449aaefa145ec3022b1b2355055313e0222a2
MrSallivan <nik.sallivan@gmail.com>
2022-11-11 22:03:49 +1000

    commit

--- JS/Basic/9.js
+++ JS/Basic/9.js
@@ -8,3 +8,21 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
+
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum + 5
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
