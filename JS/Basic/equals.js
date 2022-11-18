edd195b1ddccc1e8f3e4668bd5d52a823d87c244
MrSallivan <nik.sallivan@gmail.com>
2022-11-18 18:45:07 +1000

    Update equals.js

--- JS/Basic/equals.js
+++ JS/Basic/equals.js
@@ -32,3 +32,17 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+/////////////////////////////////////////////////
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return 'Yes'
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
