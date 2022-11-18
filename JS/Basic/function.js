3c0b5683d520279e3403ec89cba280062a4f947e
MrSallivan <nik.sallivan@gmail.com>
2022-11-18 12:04:51 +1000

    Update function.js

--- JS/Basic/function.js
+++ JS/Basic/function.js
@@ -19,3 +19,20 @@ function fun2() {
   }
   console.log(output);
 }
+
+////////////////
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item)
+  let rez = arr.shift()
+  return rez;
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
