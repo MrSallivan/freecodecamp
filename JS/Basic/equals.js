5bc86f95e6668af9c2fc9b4d35f18b15956ec2d6
MrSallivan <nik.sallivan@gmail.com>
2022-11-19 22:24:05 +1000

    Update equals.js

--- JS/Basic/equals.js
+++ JS/Basic/equals.js
@@ -95,3 +95,28 @@ if(strokes >= par + 3) {
 }
 
 golfScore(5, 4);
+//////////////////////////////////////////switch
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val){
+  case 1:
+    answer = 'alpha'
+    break
+  case 2:
+    answer = 'beta'
+    break
+  case 3:
+    answer = 'gamma'
+    break
+  case 4:
+    answer = 'delta'
+    break
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
