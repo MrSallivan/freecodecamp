188dc0444fd15453f9289fa38077f62c571a99d6
MrSallivan <nik.sallivan@gmail.com>
2022-11-20 22:51:34 +1000

    Update objects.js

--- JS/Basic/objects.js
+++ JS/Basic/objects.js
@@ -114,3 +114,17 @@ for (let i = 1; i < 10; i += 2) {
   myArray.push(i);
 }
 // Only change code below this line
+
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for(let i = 0; i < arr.length; i++){
+ for(let j = 0; j < arr[i].length; j++){
+   product *= arr[i][j]
+ }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
