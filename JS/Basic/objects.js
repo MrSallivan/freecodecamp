87231466e4490d975bc2c68f1aced7d7d81ef7a1
MrSallivan <nik.sallivan@gmail.com>
2022-11-20 14:07:39 +1000

    Update objects.js

--- JS/Basic/objects.js
+++ JS/Basic/objects.js
@@ -19,3 +19,27 @@ const myDog = {
 };
 delete myDog.tails
 // Only change code below this line
+
+//////////////////////////////////
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
+    alpha: 'Adams',
+    bravo: 'Boston',
+    charlie: 'Chicago',
+    delta: 'Denver',
+    echo: 'Easy',
+    foxtrot: 'Frank',
+  }
+
+  result = lookup[val]
+
+
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
