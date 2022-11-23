352420d6a4ed81af0038cb6b57306133cf74f729
MrSallivan <nik.sallivan@gmail.com>
2022-11-23 23:20:00 +1000

    Update destructuring.js

--- JS/Basic/destructuring.js
+++ JS/Basic/destructuring.js
@@ -77,3 +77,17 @@ const getMousePosition = (x, y) => ({ x, y });
 
 Use object property shorthand with object literals to create and return an object with name, age and gender properties.
 const createPerson = (name, age, gender) => ({name, age,gender})
+
+
+
+//////////////////////////////////////
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
